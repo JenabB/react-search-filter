ef1cf41c47e992f79674804c49f06a43f9d6664e
Yogi Surya Pranata <birulangit1996@gmail.com>
2021-02-11 12:24:51 +0700

    Updates

---
+++ precache-manifest.dfe610456378dcb0780db1bf85578a61.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb177b0e1e7f76fe86a0",
+    "url": "/react-search-filter/static/js/runtime~main.3ffa9fbc.js"
+  },
+  {
+    "revision": "51dfe3dc835355f95028",
+    "url": "/react-search-filter/static/js/main.931284da.chunk.js"
+  },
+  {
+    "revision": "9265742814159994725d",
+    "url": "/react-search-filter/static/js/2.3c5292f0.chunk.js"
+  },
+  {
+    "revision": "51dfe3dc835355f95028",
+    "url": "/react-search-filter/static/css/main.67917107.chunk.css"
+  },
+  {
+    "revision": "32ef7a96067298c6e7cdb3cd2c610996",
+    "url": "/react-search-filter/index.html"
+  }
+];
