3d6b055749bf33d43fe8cffd41442e0256d90696
JenabB <birulangit1996@gmail.com>
2021-08-06 17:42:49 +0700

    change to functional component

--- src/App.js
+++ src/App.js
@@ -1,67 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
-import Information from './info-json';
+import React, { useState } from "react";
+import Information from "./info-json";
+import "./App.css";
+const App = () => {
+  const [search, setSearch] = useState(null);
 
-class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      search: null,
-    };
-  }
-
-  searchSpace = (event) => {
-    let keyword = event.target.value;
-    this.setState({ search: keyword });
+  const searchSpace = (e) => {
+    setSearch(e.target.value);
   };
 
-  render() {
-    // eslint-disable-next-line array-callback-return
-    const items = Information.filter((data) => {
-      if (this.state.search == null) return data;
-      else if (
-        data.name.toLowerCase().includes(this.state.search.toLowerCase()) ||
-        data.category.toLowerCase().includes(this.state.search.toLowerCase()) ||
-        data.subcategory.toLowerCase().includes(this.state.search.toLowerCase())
-      ) {
-        return data;
-      }
-    }).map((data) => {
-      return (
-        <div>
-          <div className="item-container">
-            <img
-              className="product-image"
-              src={data.image}
-              alt="productimage"
-            />
-            <div className="item-description">
-              <h3>{data.name}</h3>
-              <h5>{data.price}</h5>
-              <p style={{ fontSize: '10pt' }}>stock: {data.stock}</p>
-              <h6>{data.category}</h6>
-            </div>
-          </div>
-        </div>
-      );
-    });
-
+  const items = Information.filter((data) => {
+    if (search == null) return data;
+    else if (
+      data.name.toLowerCase().includes(search.toLowerCase()) ||
+      data.category.toLowerCase().includes(search.toLowerCase()) ||
+      data.subcategory.toLowerCase().includes(search.toLowerCase())
+    ) {
+      return data;
+    }
+  }).map((data) => {
     return (
       <div>
-        <div className="app-container">
-          <input
-            type="text"
-            placeholder="Enter item to be searched"
-            onChange={(e) => this.searchSpace(e)}
-          />
-          <div className="app-body">
-            <div className="app-item">{items}</div>
+        <div className="item-container">
+          <img className="product-image" src={data.image} alt="productimage" />
+          <div className="item-description">
+            <h3>{data.name}</h3>
+            <h5>{data.price}</h5>
+            <p style={{ fontSize: "10pt" }}>stock: {data.stock}</p>
+            <h6>{data.category}</h6>
           </div>
         </div>
       </div>
     );
-  }
-}
+  });
+
+  return (
+    <div>
+      <div className="app-container">
+        <input
+          type="text"
+          placeholder="Enter item to be searched"
+          onChange={(e) => searchSpace(e)}
+        />
+        <div className="app-body">
+          <div className="app-item">{items}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
